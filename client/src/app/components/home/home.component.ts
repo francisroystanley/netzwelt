22cfc726a0ea1d7e8542cc012f56df872e5c5982
Francis Roy Stanley Falcon <francisroystanley@gmail.com>
2023-08-13 22:47:52 +0800

    Load territories from api

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 
 @Component({
@@ -7,63 +8,45 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
   existingRootTerritories: { [key: string]: boolean } = {};
-  raw_territories = [
-    {
-      id: "1",
-      name: "Metro Manila",
-      parent: null,
-    },
-    {
-      id: "101",
-      name: "Manila",
-      parent: "1",
-    },
-    {
-      id: "10101",
-      name: "Malate",
-      parent: "101",
-    },
-    {
-      id: "10102",
-      name: "Ermita",
-      parent: "101",
-    },
-    {
-      id: "10103",
-      name: "Binondo",
-      parent: "101",
-    },
-  ];
+  loading = true;
   territories: Territory[] = [];
 
+  constructor(private http: HttpClient) {}
+
   ngOnInit() {
-    const territoriesMap = this.raw_territories.reduce((obj, val) => {
-      obj[val.id] = val;
+    this.http
+      .get<{ data: RawTerritory[] }>("/api/territories")
+      .subscribe(({ data }) => {
+        const territoriesMap = data.reduce((obj, val) => {
+          obj[val.id] = val;
 
-      return obj;
-    }, {} as TerritoriesMap);
+          return obj;
+        }, {} as TerritoriesMap);
 
-    this.raw_territories.forEach((raw_territory) => {
-      let rootParent = raw_territory;
+        data.forEach((raw_territory) => {
+          let rootParent = raw_territory;
 
-      while (rootParent.parent) {
-        const newRootParent = territoriesMap[rootParent.parent];
+          while (rootParent.parent) {
+            const newRootParent = territoriesMap[rootParent.parent];
 
-        if (!newRootParent.children) newRootParent.children = [];
+            if (!newRootParent.children) newRootParent.children = [];
 
-        const isExist = newRootParent.children.some(
-          (item) => item.id === rootParent.id
-        );
+            const isExist = newRootParent.children.some(
+              (item) => item.id === rootParent.id
+            );
 
-        if (!isExist) newRootParent.children.push(rootParent);
+            if (!isExist) newRootParent.children.push(rootParent);
 
-        rootParent = newRootParent;
-      }
+            rootParent = newRootParent;
+          }
 
-      if (!this.existingRootTerritories[rootParent.id]) {
-        this.territories.push(rootParent);
-        this.existingRootTerritories[rootParent.id] = true;
-      }
-    });
+          if (!this.existingRootTerritories[rootParent.id]) {
+            this.territories.push(rootParent);
+            this.existingRootTerritories[rootParent.id] = true;
+          }
+        });
+
+        this.loading = false;
+      });
   }
 }
